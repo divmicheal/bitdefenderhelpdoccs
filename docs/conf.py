e4c9ab86f643feb59bd0515bf1ae064a73199f38
divmicheal <divmicheal@outlook.com>
2026-02-25 12:29:00 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'bitdefender-helpdpcs'
+project = 'bitdefender-helpdocs'
 author = 'Documentation Team'
 release = '1.0'
 
@@ -16,5 +16,11 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# VERY IMPORTANT FOR DEFAULT READTHEDOCS URL
+# IMPORTANT: Default ReadTheDocs URL
 html_baseurl = "https://bittdefender.readthedocs.io/en/latest/"
+
+# ✅ GOOGLE + BING VERIFICATION META TAG
+html_meta = {
+    "google-site-verification": "Cva8KgvW-eQpRtsdf8vIcSb023IJtLJfC8PxJAlQ0mc",
+    "msvalidate.01": "739245F5D54BCBF40AC056DC0CBF5710"
+}
